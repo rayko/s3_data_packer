a10257a6395d7bd2c2150ddda3154c1915ff2b71
Rayko <rayko.drg@gmail.com>
2021-08-24 13:51:49 -0300

    Rework

--- lib/s3_data_packer.rb
+++ lib/s3_data_packer.rb
@@ -1,5 +1,18 @@
+require 'csv'
+require 'json'
+
 require "s3_data_packer/version"
 require 's3_data_packer/configuration'
+require 's3_data_packer/package'
+require 's3_data_packer/packer'
+
+require 's3_data_packer/packages/csv'
+require 's3_data_packer/packages/json'
+
+require 's3_data_packer/sources/directory'
+require 's3_data_packer/sources/bucket'
+
+require 's3_data_packer/targets/s3'
 
 module S3DataPacker
   class << self
@@ -14,6 +27,10 @@ module S3DataPacker
     def configure
       yield configuration
       self.configuraiton
+    end
+
+    def logger
+      @logger ||= config.logger || Logger.new('log/s3_data_packer.log')
     end
   end
 end
