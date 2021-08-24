00d620d0c4041d00665cc50791e91dbb44006a99
Rayko <rayko.drg@gmail.com>
2021-08-24 12:38:48 -0300

    Setting up some configs for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "s3_data_packer"
 
+WORKDIR = 'tmp/test_data'
+
+unless Dir.exist?(WORKDIR)
+  Dir.mkdir 'tmp' unless Dir.exist?('tmp')
+  Dir.mkdir 'tmp/test_data'
+end
+
 require "minitest/autorun"
