c23e5995bcf8202289b0cf9ceefc9ef62529cf90
Rayko <rayko.drg@gmail.com>
2021-08-31 16:57:13 -0300

    Adding queue class for workers

---
+++ lib/s3_data_packer/queue.rb
@@ -0,0 +1,42 @@
+module S3DataPacker
+  class Queue
+    attr_reader :total_items, :items
+
+    def initialize opts = {}
+      @max_items = opts[:max_items]
+      @wait_time = opts[:wait_time]
+      @total_items = 0
+    end
+
+    def max_items
+      @max_items ||= S3DataPacker.config.max_queue_size
+    end
+
+    def wait_time
+      @wait_time ||= S3DataPacker.config.max_queue_wait
+    end
+
+    def items
+      @items ||= []
+    end
+
+    def add!(item)
+      items << item
+      @total_items += 1
+      sleep(wait_time) if size >= max_items
+    end
+
+    def fetch!
+      items.shift
+    end
+
+    def size
+      items.size
+    end
+
+    def reset!
+      @items = []
+      @total_items = 0
+    end
+  end
+end
