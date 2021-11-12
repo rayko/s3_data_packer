46108a53296ebd2b6fe55d4252464643c91dd175
Rayko <rayko.drg@gmail.com>
2021-11-12 16:12:01 -0300

    Configuring simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,18 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "s3_data_packer"
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test/"
+end
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'minitest/autorun'
+require 'fake_s3'
 require 'byebug'
 
+require 's3_data_packer'
 WORKDIR = 'tmp/test_data'
 
 unless Dir.exist?(WORKDIR)
   Dir.mkdir 'tmp' unless Dir.exist?('tmp')
   Dir.mkdir 'tmp/test_data'
 end
-
-
-
-require "minitest/autorun"
-require 'fake_s3'
 
